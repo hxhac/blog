2546b97d06c1931a5ecbac3392713878bc290713
x1a0xf <yyzw@live.com>
2024-04-23 17:47:49 +0800

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://hxhac.github.io/blog/',
+  url: 'https://blog.hxha.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
